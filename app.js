c298d9c8604155c57cbf1ce4d3d496a123f0d0f9
Ahmed EL-Dakhakhny <>
2020-04-03 12:59:13 +0200

    rendering with pug

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const pug
+const pug = require('pug');
 const app = new express();
 const port = process.argv[2];
 app.use(express.static(process.argv[3] || 'public'));
+app.set('view engine', 'pug')
 app.get('/home',function(req,res){
-    res.send('Hello World!');
+    res.render(process.argv[3]||'index', {date: new Date().toDateString()})
 });
 app.listen(port); 
