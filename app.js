b112dda92252f02de24edd6d3fcac752fd992ef4
Ahmed EL-Dakhakhny <>
2020-04-04 05:28:25 +0200

    rendering views fro templates

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 const pug = require('pug');
 const app = new express();
 const port = process.argv[2];
 app.use(express.static(process.argv[3] || 'public'));
+app.set('views', path.join(__dirname, 'templates'))
 app.set('view engine', 'pug')
 app.get('/home',function(req,res){
-    res.render(process.argv[3]||'index', {date: new Date().toDateString()})
+    res.render(process.argd||'index', {date: new Date().toDateString()})
 });
 app.listen(port); 
