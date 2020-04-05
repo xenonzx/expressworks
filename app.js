da81f79477e1c20cf1034ca83f220705603dd927
Ahmed EL-Dakhakhny <>
2020-04-05 21:35:47 +0200

    parsing post body

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const path = require('path');
 const pug = require('pug');
 const app = new express();
 const port = process.argv[2];
+const bodyParser = require('body-parser');
+
 app.use(express.static(process.argv[3] || 'public'));
-app.set('views', path.join(__dirname, 'templates'))
-app.set('view engine', 'pug')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'pug');
 app.get('/home',function(req,res){
     res.render(process.argd||'index', {date: new Date().toDateString()})
 });
+app.post('/form', function(req,res){
+    //str is passed by the form
+    res.send(req.body.str.split('').reverse().join(''));
+    
+    //req.body.str
+})
 app.listen(port); 
