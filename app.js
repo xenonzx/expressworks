316d4bc94f82bc5e99735cb13dda0954324f50b6
Ahmed EL-Dakhakhny <>
2020-04-09 13:12:43 +0200

    sending the contents of the file in /books

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const pug = require('pug');
+const crypto = require('crypto');
+const fs = require('fs');
 const app = new express();
 const port = process.argv[2];
+const filePath = process.argv[3];
 const bodyParser = require('body-parser');
 
 app.use(express.static(process.argv[3] || 'public'));
@@ -16,10 +19,30 @@ app.set('view engine', 'pug');
 app.get('/home',function(req,res){
     res.render(process.argd||'index', {date: new Date().toDateString()})
 });
+
+app.put('/message/:ID', function(req, res){
+    const id = req.params.ID;
+    res.send(crypto.createHash('sha1')
+      .update(new Date().toDateString() + id)
+      .digest('hex'));
+});
+
 app.post('/form', function(req,res){
     //str is passed by the form
     res.send(req.body.str.split('').reverse().join(''));
-    
-    //req.body.str
-})
+});
+
+app.get('/books', function(req, res){
+    fs.readFile(filePath, function(err, data){
+        if(err){
+            console.log(err);
+        }
+
+        if (data){
+            try {
+                books = JSON.parse(data.toString());
+            res.json( JSON.parse(data.toString()));
+        }
+    })
+});
 app.listen(port); 
